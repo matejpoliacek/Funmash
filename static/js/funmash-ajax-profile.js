e99cd7eff338d4b0e13f2d3fd30bee41a3f0a8a1
Peter Vanik <retepvan@yahoo.co.uk>
2017-03-20 18:04:47 +0000

    Success fadeout and options button div link fix

--- static/js/funmash-ajax-profile.js
+++ static/js/funmash-ajax-profile.js
@@ -28,6 +28,7 @@ $(document).ready(function (e) {
 			
         });
 		document.getElementById("ifsuccess").innerHTML = "succesfuly uploaded!";
+		success_fadeout();
     }));
 
     $("#ImageBrowse").on("change", function() {
@@ -59,4 +60,17 @@ $(document).ready(function (e) {
 			$('#uploaded').html(data);
 	});
 	});
+
+	 function success_fadeout() {
+  		var fade = document.getElementById("fade"); // get required element
+  		fade.style.opacity = 1; // set opacity for the element to 1
+  		var timerId = setInterval(function() { // start interval loop
+	    var opacity = fade.style.opacity; // get current opacity
+	    if (opacity == 0) { // check if its 0 yet
+	      clearInterval(timerId); // if so, exit from interval loop
+	    } else {
+	      fade.style.opacity = opacity - 0.05; // else remove 0.04 from opacity
+	    }
+	  }, 100); // run every 0.1 second
+	}
 });
--- static/js/profile_img.js
+++ static/js/profile_img.js
@@ -24,21 +24,7 @@ function zpopup() {
             body: '<div class="w2ui-centered"><img src="'+getImageName()+'"></img></div>'
         });
     }
-	
-	
-//fadeout
-setTimeout(function() { // start a delay
-  var fade = document.getElementById("fade"); // get required element
-  fade.style.opacity = 1; // set opacity for the element to 1
-  var timerId = setInterval(function() { // start interval loop
-    var opacity = fade.style.opacity; // get current opacity
-    if (opacity == 0) { // check if its 0 yet
-      clearInterval(timerId); // if so, exit from interval loop
-    } else {
-      fade.style.opacity = opacity - 0.05; // else remove 0.05 from opacity
-    }
-  }, 100); // run every 0.1 second
-}, 3000)
+
 
 	/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
@@ -53,3 +39,19 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.body.style.backgroundColor = "#ffe29b";
 }
+
+
+// Matej's original fadeout which Peter modified for success_fadeout in funmash-ajax-profile.js
+//fadeout
+// setTimeout(function() { // start a delay
+//   var fade = document.getElementById("fade"); // get required element
+//   fade.style.opacity = 1; // set opacity for the element to 1
+//   var timerId = setInterval(function() { // start interval loop
+//     var opacity = fade.style.opacity; // get current opacity
+//     if (opacity == 0) { // check if its 0 yet
+//       clearInterval(timerId); // if so, exit from interval loop
+//     } else {
+//       fade.style.opacity = opacity - 0.05; // else remove 0.05 from opacity
+//     }
+//   }, 100); // run every 0.1 second
+// }, 3000)
