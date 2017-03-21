c995f54065e8e024a126b36a6627f583cb942413
Peter Vanik <retepvan@yahoo.co.uk>
2017-03-21 19:27:45 +0000

    carousel working about

--- static/js/about.js
+++ static/js/about.js
@@ -1,4 +1,4 @@
-
+// $(document).ready(function (e) {
 
 	/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
@@ -26,6 +26,24 @@ jQuery(function () {
     }, 3500)
 })
 
+// Leo text //
+
+// $('#leo').on('click',(function(e) {
+// 	var fade = document.getElementById("leotext"); // get required element
+//   		fade.style.opacity = 1; // set opacity for the element to 1
+// 	document.getElementById("leotext").innerHTML = "Cheers for the feedback!";
+// 	var timerId = setInterval(function() { // start interval loop
+// 	    var opacity = fade.style.opacity; // get current opacity
+// 	    if (opacity == 0) { // check if its 0 yet
+// 	      clearInterval(timerId); // if so, exit from interval loop
+// 	    } else {
+// 	      fade.style.opacity = opacity - 0.05; // else remove 0.04 from opacity
+// 	    }
+// 	  }, 100); // run every 0.1 second
+// 	document.getElementById("leotext").innerHTML = "Tired of scrolling through images? Just click the one you find funnier - and surprise yourself with what comes next. ";
+// 	}));
+
+// });
 
 // Matej's original fadeout which Peter modified for success_fadeout in funmash-ajax-profile.js
 //fadeout
