b98aa32bd3a430a156fa4c1ca0e479421112f938
Assandra <assandra123@24dcaea7d349>
2017-03-23 13:58:07 +0000

    Last commit

--- funmash/urls.py
+++ funmash/urls.py
@@ -31,6 +31,7 @@ class MyRegistrationView(RegistrationView):
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^funmash_app/', include('funmash_app.urls')),
+    url(r'^accounts/password/change/done/$', views.index, name='index'),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
     url(r'^admin/', admin.site.urls),
